7e11a2195ce93ede0f605aefb49dbb48a2ae424c
Manon <mmanon37270@gmail.com>
2022-04-09 12:11:52 +0200

    BBD first version

---
+++ app/src/main/java/com/example/biblio_tech/DBContract.java
@@ -0,0 +1,20 @@
+package com.example.biblio_tech;
+
+public class DBContract {
+    public static class Favori {
+        public static final String TABLE_NAME = "favorite";
+        public static final String _ID_LIVRE = "id";
+    }
+    public static class Lu {
+        public static final String TABLE_NAME = "lu";
+        public static final String _ID_LIVRE = "id";
+    }
+    public static class EnCours {
+        public static final String TABLE_NAME = "enCours";
+        public static final String _ID_LIVRE = "id";
+    }
+    public static class ALire {
+        public static final String TABLE_NAME = "aLire";
+        public static final String _ID_LIVRE = "id";
+    }
+}
---
+++ app/src/main/java/com/example/biblio_tech/Response.java
@@ -0,0 +1,20 @@
+package com.example.biblio_tech;
+
+public class Response {
+
+    private  int m_id;
+
+    public Response (int i){
+       setM_id(i);
+    }
+
+    public int getM_id() {
+        return m_id;
+    }
+
+    public void setM_id(int m_id) {
+        this.m_id = m_id;
+    }
+
+}
+
