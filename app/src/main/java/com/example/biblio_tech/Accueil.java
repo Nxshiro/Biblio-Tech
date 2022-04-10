5ff51bf255b06812c42cab5543e2774d69195083
Manon <mmanon37270@gmail.com>
2022-04-10 19:07:03 +0200

    essai fragment

--- app/src/main/java/com/example/biblio_tech/Accueil.java
+++ app/src/main/java/com/example/biblio_tech/Accueil.java
@@ -1,6 +1,8 @@
 package com.example.biblio_tech;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.graphics.Color;
@@ -16,10 +18,20 @@ public class Accueil extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.fragment, new ListeFragment());
+        ft.commit();
     }
     public void search(View v){
         Intent i = new Intent (Accueil.this, Liste.class);
         startActivity(i);
     }
 
+    public void goTo(@NonNull View v) {
+        if (v.getId()==R.id.bfav || v.getId()==R.id.blire || v.getId()==R.id.blu||v.getId()==R.id.bcours){
+            Intent i = new Intent(this, ListeFragment.class);
+            startActivity(i);
+        }
+    }
 }
